01b481ac5d22eebeaf5b03606154c75596d7d238
Bintang Ramadhan <mbintangr13@gmail.com>
2025-05-24 14:10:57 +0700

    feat: assign collector

--- dashboard/pages/user.py
+++ dashboard/pages/user.py
@@ -28,34 +28,40 @@ response = requests.request(
 
 
 if response.status_code == 200:
-    data = response.json()
+    if len(response.json()) == 0:
+        data = []
+        st.error("Data Locker Kosong")
+    else:
+        data = response.json()
 
 # Layout
 
 st.title("Welcome, %s " % st.session_state.user_name)
-st.markdown("## Terima kasih sudah menggunakan jasa locker kami")
-st.divider()
-st.markdown("## Berikut adalah QR Code locker Anda")
-st.markdown("Harap simpan QRCode dengan baik dengan cara SS atau klik button Download QRCode")
-st.markdown("Berikut adalah Loker yang tersedia")
+st.markdown("Terima kasih sudah menggunakan jasa locker kami 🙏🏻")
+# st.divider()
 
 cols = st.columns(5, gap="large",vertical_alignment="center")  # Ubah jadi 4 atau 5 jika ingin lebih rapat
 
 for idx, item in enumerate(data):
     id_value = item.get("id")
     id_locker = item.get("locker_id")
+    if id_value is not None:
+        st.markdown("## Berikut adalah QR Code locker Anda")
+        st.markdown("Harap simpan QR Code dengan baik dengan cara Screenshot")
+        st.markdown("Berikut adalah Loker yang tersedia")
+        
+        qr_img = qrcode.make(str(id_value))
+        buf = io.BytesIO()
+        qr_img.save(buf, format="PNG")
+        buf.seek(0)
 
-    qr_img = qrcode.make(str(id_value))
-    buf = io.BytesIO()
-    qr_img.save(buf, format="PNG")
-    buf.seek(0)
+        col = cols[idx % 2]
+        with col:
+            with st.container(border=True):
+                st.image(buf, caption=f"QR Code Locker: {id_locker}", width=265)
+                st.caption(f"ID: {id_value}")
 
-    col = cols[idx % 2]
-    with col:
-        with st.container(border=True):
-            st.image(buf, caption=f"QR Code Locker: {id_locker}", width=265)
-            st.caption(f"ID: {id_value}")
-
-    if (idx + 1) % 2 == 0 and (idx + 1) < len(data):
-        cols = st.columns(3)
-        st.markdown("#")
+        if (idx + 1) % 2 == 0 and (idx + 1) < len(data):
+            cols = st.columns(3)
+    else:
+        st.warning('Anda belum memiliki locker')
